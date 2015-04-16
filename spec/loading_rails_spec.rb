31c7b8722bc3b686ba2a98246f81161f86d7f9eb
Aaron Kromer <aaron.kromer@gmail.com>
2015-04-16 12:20:53 -0400

    Add two specs per repo steps
    See https://github.com/rspec/rspec-rails/issues/1355#issuecomment-93762678

---
+++ spec/loading_rails_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "This loads Rails" do
+  it "has access to Rails" do
+    expect(Rails).to be
+  end
+end
---
+++ spec/standard_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe "This does not use Rails" do
+  it "passes" do
+    expect(true).to be
+  end
+end
